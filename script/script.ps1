4ea40933412c3815c39144799b7c1234fc741e1f
CrystalBose <74994699+CrystalBose@users.noreply.github.com>
2020-12-08 01:50:32 +0000

    Update script.ps1

--- script/script.ps1
+++ script/script.ps1
@@ -1,6 +1,7 @@
-$SCRIPT_HEAD = git ls-remote https://github.com/${env:SCRIPT}/appveyor-git.git HEAD | awk '{ print $1}'
+$SCRIPT_HEAD = git ls-remote https://github.com/${env:SCRIPT}/appveyor-ci.git HEAD | awk '{ print $1}'
 
-$env:SCRIPT = "https://github.com/${env:SCRIPT}/appveyor-git/raw/${SCRIPT_HEAD}"
+
+$env:SCRIPT = "https://github.com/${env:SCRIPT}/appveyor-ci/raw/${SCRIPT_HEAD}"
 
 
 
