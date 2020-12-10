9bf009ac92b16234c7b4ae2e324ee2fc9204e61d
CrystalBose <74994699+CrystalBose@users.noreply.github.com>
2020-12-10 02:05:43 +0000

    Update squash.ps1

--- squash/squash.ps1
+++ squash/squash.ps1
@@ -38,14 +38,18 @@ if( $MERGE -Ge $MAX ) {
 $MESSAGE = git log -1 --skip=( $MERGE-1 ) --pretty=format:'%s%n%b'
 
 
-
+echo "3a"
 if( $MERGE -Eq $MAX) {
+  echo "3b"
   git reset --soft HEAD~( $MERGE-1 )
   git commit --amend -m "$MESSAGE" --author="${env:GIT_AUTHOR} <${env:GIT_EMAIL}>"
 }
 
 
+echo "3c"
 elseif( $MERGE -Ge 1 ) {
   git reset --soft HEAD~$MERGE
+  echo "3d"
   git commit -m "$MESSAGE" --author="${env:GIT_AUTHOR} <${env:GIT_EMAIL}>"
+  echo "3e"
 }
