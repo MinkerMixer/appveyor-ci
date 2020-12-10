0f8e04b5156340097370d4ce6d7ed405c45bda81
CrystalBose <74994699+CrystalBose@users.noreply.github.com>
2020-12-10 02:00:47 +0000

    Update script.ps1

--- script/script.ps1
+++ script/script.ps1
@@ -1,8 +1,8 @@
-$GIT_SCRIPT_HEAD = git ls-remote https://github.com/${env:GIT_SCRIPT}/appveyor-ci.git HEAD | awk '{ print $1}'
+$GIT_SCRIPT_HEAD = git ls-remote https://github.com/${env:GIT_SCRIPT}/appveyor-git.git HEAD | awk '{ print $1}'
 
 
-$env:GIT_SCRIPT = "https://github.com/${env:GIT_SCRIPT}/appveyor-ci/raw/${GIT_SCRIPT_HEAD}"
+$env:GIT_SCRIPT = "https://github.com/${env:GIT_SCRIPT}/appveyor-git/raw/${GIT_SCRIPT_HEAD}"
 $env:PATH = ${env:APPVEYOR_BUILD_FOLDER} + ";" + ${env:PATH}
 
 
-echo "`n`nCI System: ${env:APPVEYOR_BUILD_WORKER_IMAGE}`n`n"
+echo "`n`n`nCI System: ${env:APPVEYOR_BUILD_WORKER_IMAGE}`n`n"
