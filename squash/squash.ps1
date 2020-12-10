94a4473596d64aeabb01f42ff2077f288646b34b
CrystalBose <74994699+CrystalBose@users.noreply.github.com>
2020-12-10 01:59:42 +0000

    Update squash.ps1

--- squash/squash.ps1
+++ squash/squash.ps1
@@ -1,15 +1,20 @@
+$MERGE = 0
+$MAX = git rev-list --count HEAD
+
+
+
+###################################################
+
+
+
 if( ${env:GIT_SQUASH} -Ge 0 ) {
   $MERGE = ${env:GIT_SQUASH}
 }
 
 
 else {
-  $MERGE = 0
-  $MAX = git rev-list --count HEAD
-
-
   while( $MERGE -Lt $MAX ) {
-    if(( git log -1 --skip=$MERGE --pretty=format:'%an' ) -Match ${env:APPVEYOR_REPO_COMMIT_AUTHOR} -Eq $False ) {
+    if(( git log -1 --skip=$MERGE --pretty=format:'%an' ) -Match ${env:GIT_AUTHOR} -Eq $False ) {
       break
     }
 
@@ -20,20 +25,27 @@ else {
 
 
 
-###################################################
-
-
-
 if( $MERGE -Ge $MAX ) {
-  $MERGE = $MAX - 1
+  $MERGE = $MAX
 }
 
 
 
-if( $MERGE -Ge 1 ) {
-  $TITLE = git log -5 --skip=$MERGE --pretty=format:'%s%n%b'
+###################################################
 
 
+
+$MESSAGE = git log -1 --skip=( $MERGE-1 ) --pretty=format:'%s%n%b'
+
+
+
+if( $MERGE -Eq $MAX) {
+  git reset --soft HEAD~( $MERGE-1 )
+  git commit --amend -m "$MESSAGE" --author="${env:GIT_AUTHOR} <${env:GIT_EMAIL}>"
+}
+
+
+elseif( $MERGE -Ge 1 ) {
   git reset --soft HEAD~$MERGE
-  # git push -f -m "$TITLE" --author="${env:GIT_AUTHOR} <${env:GIT_EMAIL}>"
+  git commit -m "$MESSAGE" --author="${env:GIT_AUTHOR} <${env:GIT_EMAIL}>"
 }
